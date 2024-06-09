0a9b31a3c486de094d0310370de75a1f950673d5
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-06-09 21:44:06 +0900

    Add files via upload

---
+++ src/main/java/com/springboot/RestDocsApplication.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class RestDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDocsApplication.class, args);
+	}
+
+}
